088cb3758024d940cbe3e9eeae698d8cf3391685
Dan Nilsson <raxptor@gmail.com>
2016-02-18 13:01:03 +0100

    - stuff

---
+++ node/server.js
@@ -0,0 +1,24 @@
+var finalhandler = require('finalhandler')
+var http         = require('http')
+var Router       = require('router')
+ 
+var router = Router()
+var api = Router()
+router.use('/api/', api);
+
+router.get('/', function (req, res) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  res.end('Hello World!')
+})
+
+api.get('/token/:token', function(req, res) {
+	console.log(req.params)
+	res.end('Yes [' + req.params.token + "]")
+})
+
+ 
+var server = http.createServer(function(req, res) {
+  router(req, res, finalhandler(req, res))
+})
+ 
+server.listen(8888)
